b6cba6550d4408e925795b7dd3a03e206ffa1585
abhimanyu <abhimanyu.s.chaudhary@gmail.com>
2018-11-02 15:37:30 +0530

    save and load works

--- population.py
+++ population.py
@@ -1,4 +1,4 @@
-import chromosome
+from chromosome import link, chromosome
 import _pickle as cPickle
 from copy import deepcopy
 class population:
@@ -7,9 +7,10 @@ class population:
 		self.numberOfIndividuals = N
 		self.individuals = []
 		for i in range(self.numberOfIndividuals):
-			self.individuals.append(chromosome())
+			temp = deepcopy(chromosome())
+			self.individuals.append(temp)
 	def save(self):
-		pickle_out = open("savedPopulation/generation"+str(self.generationNumber)+".gen", "wb")
+		pickle_out = open("savedPopulations/generation"+str(self.generationNumber)+".gen", "wb+")
 		cPickle.dump(self, pickle_out)
 	def copy(self, other):
 		self.generationNumber = deepcopy(other.generationNumber)
@@ -17,9 +18,20 @@ class population:
 		self.individuals = deepcopy(other.individuals)
 	def load(self, generationNumber):
 		#this initializes first and then loads, can make it efficient
-		pickle_in = open("savedPopulation/generation"+str(generationNumber)+".gen", "rb")
-		other = pickle.load(pickle_in)
+		pickle_in = open("savedPopulations/generation"+str(generationNumber)+".gen", "rb")
+		other = cPickle.load(pickle_in)
 		self.generationNumber = deepcopy(other.generationNumber)
 		self.numberOfIndividuals = deepcopy(other.numberOfIndividuals)
 		self.individuals = deepcopy(other.individuals)
+	def printPopulation(self):
+		print(self.generationNumber, self.numberOfIndividuals)
+
+# p = population(10)
+# p.printPopulation()
+# p.save()
+p = population(1)
+p.printPopulation()
+p.load(0)
+p.printPopulation()
+
 
